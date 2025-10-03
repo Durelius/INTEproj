d9696c21ae5db2c11066b5e028795a208e0123e0
Carl Thomasson <thoma@Carls-MacBook-Air.local>
2025-10-03 12:52:36 +0200

    Added baseline Item class

--- item/model.go
+++ item/model.go
@@ -9,13 +9,31 @@ type BaseItem struct {
 	id       string
 	weight   int
 	name     string
-	itemType string
+	itemType ItemType
 }
+type ItemType string
 
-func New(name string, weight int, itemType string) (item, error) {
+const (
+	Stick  ItemType = "Stick"
+	Food   ItemType = "Food"
+	Potion ItemType = "Potion"
+)
+
+func New(name string, weight int, itemType ItemType) (*BaseItem, error) {
 	if len(name) == 0 {
 		return nil, fmt.Errorf("No name supplied")
 	}
+	if weight < 0 {
+		return nil, fmt.Errorf("Weight cannot be negative")
+	}
 
-	return &BaseCharacter{id: random.String(), health: 100, name: name}, nil
+	return &BaseItem{id: random.String(), itemType: itemType, name: name}, nil
+}
+
+func (c *BaseItem) GetID() string {
+	return c.id
+}
+
+func (c *BaseItem) getWeight() int {
+	return c.weight
 }
