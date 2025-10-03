807a1df0d69c520965d4ca550acaf1a4ec4a6da6
Carl Thomasson <thoma@Carls-MacBook-Air.local>
2025-10-03 12:25:03 +0200

    refactored random

--- assets/item/model.go
+++ assets/item/model.go
@@ -1,7 +1,21 @@
 package item
 
-type BaseCharacter struct {
-	id     string
-	health int
-	name   string
+import (
+	"INTE/projekt/random"
+	"fmt"
+)
+
+type BaseItem struct {
+	id       string
+	weight   int
+	name     string
+	itemType string
+}
+
+func New(name string, weight int, itemType string) (item, error) {
+	if len(name) == 0 {
+		return nil, fmt.Errorf("No name supplied")
+	}
+
+	return &BaseCharacter{id: random.String(), health: 100, name: name}, nil
 }
