245413f8715ce431895ea16254a813ea7cdbc347
Durelius <wilhelm@durelius.se>
2025-10-03 11:14:02 +0200

    const for id length

--- character/model.go
+++ character/model.go
@@ -11,12 +11,14 @@ type Character struct {
 	name   string
 }
 
+const id_length = 16
+
 func New(name string) (*Character, error) {
 	if len(name) == 0 {
 		return nil, fmt.Errorf("No name supplied")
 	}
 
-	return &Character{id: random.String(16), health: 100, name: name}, nil
+	return &Character{id: random.String(id_length), health: 100, name: name}, nil
 }
 
 func (c *Character) GetID() string {
