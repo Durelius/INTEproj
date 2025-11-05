0d8af0cb2df15ab958de7783a37e7c08fb8afbb3
Carl Thomasson <cath8913@dsv.su.se>
2025-11-05 19:20:21 +0100

    refactoring and working how tests are built in its folder

---
+++ test/Item/rarity_test.go
@@ -0,0 +1,43 @@
+package itemtest
+
+import (
+	"testing"
+
+	"github.com/Durelius/INTEproj/internal/item"
+)
+
+// Full Random Item
+func TestRandomItemsCreation(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		v := item.GetRandomItem()
+
+		if _, ok := v.(item.Item); !ok {
+			t.Errorf("expected type item.Item, got %T", v)
+		}
+	}
+}
+
+// Check Rarity IOTA works correctly
+func TestCreateOutOfBoundsIota(t *testing.T) {
+	for i := 4; i < 10; i++ {
+		v := item.GetRandomItemByRarity(item.Rarity(i))
+		if _, ok := v.(item.Item); ok {
+			t.Errorf("expected nil, got %T", v)
+		}
+	}
+}
+
+// Checks that all normal rarities can be created
+func TestGetRandomItemByRarity(t *testing.T) {
+
+	for _, rarity := range []item.Rarity{item.Common, item.Rare, item.Epic, item.Legendary} {
+		got := item.GetRandomItemByRarity(rarity)
+		if got == nil {
+			t.Errorf("Expected item for rarity %v, got nil", rarity)
+			continue
+		}
+		if got.GetRarity() != rarity {
+			t.Errorf("Expected rarity %v, got %v for item %v", rarity, got.GetRarity(), got.GetName())
+		}
+	}
+}
