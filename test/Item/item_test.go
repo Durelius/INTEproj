2195b924e50aa8fefdc7cab908ee4d0a3cff27e0
Carl Thomasson <cath8913@dsv.su.se>
2025-11-06 08:14:35 +0100

    final changes

--- test/Item/item_test.go
+++ test/Item/item_test.go
@@ -4,7 +4,17 @@ import (
 	"testing"
 
 	"github.com/Durelius/INTEproj/internal/item"
+	"github.com/Durelius/INTEproj/internal/random"
+	"github.com/onsi/gomega"
 )
+
+func TestRandomItemsRate(t *testing.T) {
+	getRandomItemsWith(int64(5), t)
+	getRandomItemsWith(int64(6), t)
+	getRandomItemsWith(int64(7), t)
+	getRandomItemsWith(int64(8), t)
+	getRandomItemsWith(int64(9), t)
+}
 
 // Testar random delningen mellan alla olika rareities.
 // Common 50 %
@@ -12,7 +22,9 @@ import (
 // Epic 8 %
 // Legendary 2%
 
-func TestGetRandomItemsWith(t *testing.T) {
+func getRandomItemsWith(seed int64, t *testing.T) {
+	g := gomega.NewWithT(t)
+	random.SetSeed(seed)
 	length := 1000
 	items := []item.Item{}
 	for i := 0; i < length; i++ {
@@ -23,4 +35,17 @@ func TestGetRandomItemsWith(t *testing.T) {
 	for _, it := range items {
 		counts[it.GetRarity()]++
 	}
+
+	g.Expect(counts[item.Common]).To(gomega.BeNumerically(">=", 470))
+	g.Expect(counts[item.Common]).To(gomega.BeNumerically("<=", 530))
+
+	g.Expect(counts[item.Rare]).To(gomega.BeNumerically(">=", 370))
+	g.Expect(counts[item.Rare]).To(gomega.BeNumerically("<=", 430))
+
+	g.Expect(counts[item.Epic]).To(gomega.BeNumerically(">=", 70))
+	g.Expect(counts[item.Epic]).To(gomega.BeNumerically("<=", 90))
+
+	g.Expect(counts[item.Legendary]).To(gomega.BeNumerically(">=", 15))
+	g.Expect(counts[item.Legendary]).To(gomega.BeNumerically("<=", 25))
+
 }
